ae539127cd4db8b28f1df63db6dece62108fde07
Nathan Miller <nmiller217@gmail.com>
2025-06-25 23:46:49 -0700

    add cdktf classes to sharedlib

---
+++ SharedLib/Cdktf/CdktfProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Constructs;
+using HashiCorp.Cdktf;
+
+namespace AgentService.Cdktf
+{
+    public class CdktfProxy
+    {
+        public int RunCommand(string command, params string[] args)
+        {
+            // uses 
+            return -1;
+        }
+
+        public int CreateDockerContainer(string image)
+        {
+            App app = new App();
+            var dockerContainer = new DockerContainer(app, "learn-cdktf-docker");
+            app.Synth();
+            Console.WriteLine("App synth complete");
+
+            return -1;
+        }
+    }
+
+}
---
+++ SharedLib/Cdktf/DockerContainer.cs
@@ -0,0 +1,36 @@
+﻿
+using Constructs;
+using HashiCorp.Cdktf;
+
+using HashiCorp.Cdktf.Providers.Docker.Provider;
+using HashiCorp.Cdktf.Providers.Docker.Image;
+using HashiCorp.Cdktf.Providers.Docker.Container;
+
+namespace AgentService.Cdktf
+{
+    internal class DockerContainer : TerraformStack
+    {
+        public DockerContainer(Construct scope, string id) : base(scope, id)
+        {
+
+            new DockerProvider(this, "docker", new DockerProviderConfig { });
+
+            Image dockerImage = new Image(this, "nginxImage", new ImageConfig
+            {
+                Name = "nginx:latest",
+                KeepLocally = false,
+            });
+
+            new Container(this, "nginxContainer", new ContainerConfig
+            {
+                Image = dockerImage.Name,
+                Name = "tutorial",
+                Ports = new[] { new ContainerPorts 
+                {
+                Internal = 80,
+                External = 8000
+            }}
+            });
+        }
+    }
+}
